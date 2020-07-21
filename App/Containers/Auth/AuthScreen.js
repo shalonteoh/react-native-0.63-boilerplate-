c53e77263ecb0b893f57c44689bf8631f06cf4b6
Shalon Teoh <shalon@exabytes.com>
2020-07-21 18:56:26 +0800

    feat(auth): Add apple login to auth screen

--- App/Containers/Auth/AuthScreen.js
+++ App/Containers/Auth/AuthScreen.js
@@ -11,6 +11,12 @@ import {
 import { googleClientId } from "../../../app.json"
 import Actions from 'App/Stores/Auth/Actions'
 import Toast from 'react-native-simple-toast'
+import appleAuth, {
+    AppleButton,
+    AppleAuthRequestOperation,
+    AppleAuthRequestScope,
+    AppleAuthCredentialState,
+} from '@invertase/react-native-apple-authentication'
 
 GoogleSignin.configure({
     webClientId: googleClientId, // client ID of type WEB for your server (needed to verify user ID and offline access)
@@ -45,12 +51,49 @@ class AuthScreen extends Component {
             }
         }
     };
+
+    onAppleButtonPress = async () => {
+        try {
+            // performs login request
+            const appleAuthRequestResponse = await appleAuth.performRequest({
+                requestedOperation: AppleAuthRequestOperation.LOGIN,
+                requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
+            });
+
+            // get current authentication state for user
+            // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+            const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+
+            // use credentialState response to ensure the user is authenticated
+            if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
+                // user is authenticated
+                console.log(appleAuthRequestResponse);
+            }
+        } catch (error) {
+            Toast.show(error);
+        }
+    }
     render() {
         return (
             <View style={[Helpers.fillRowCenter, styles.container]}>
-                <View style={[Helpers.center]}>
+                <View>
+                    <Text>Login/Sign Up</Text>
+                    <AppleButton
+                        buttonStyle={AppleButton.Style.BLACK}
+                        buttonType={AppleButton.Type.SIGN_IN}
+                        style={{
+                            width: 200,
+                            height: 46,
+                            marginVertical: 5,
+                        }}
+                        onPress={this.onAppleButtonPress}
+                    />
                     <GoogleSigninButton
-                        style={{ width: 200, height: 48 }}
+                        style={{
+                            width: 200,
+                            height: 48,
+                            marginVertical: 5,
+                        }}
                         size={GoogleSigninButton.Size.Wide}
                         color={GoogleSigninButton.Color.Dark}
                         onPress={this.signIn}
