50a0c0397a83053861816644a3ecdc7e7fd928f6
Shalon Teoh <shalon@exabytes.com>
2020-07-25 19:46:09 +0800

    feat(history): Add last 7 days date range ui

--- App/Containers/History/HistoryScreen.js
+++ App/Containers/History/HistoryScreen.js
@@ -1,18 +1,72 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
 import { Helpers, Metrics } from 'App/Theme';
 import styles from './indexStyle';
-
+import moment from 'moment-timezone';
+import { getTimeZone } from "react-native-localize";
 export default class HistoryScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            last7days: [],
+            selectedDate: 0,
+        }
+    }
+    componentDidMount() {
+        // Get last 7 days
+        this.getLast7Days();
+    }
+
+    getLast7Days = () => {
+        let start = moment().tz(getTimeZone()).subtract(6, 'days').startOf('day');
+        let end = moment().tz(getTimeZone()).format('YYYY-MM-DD');
+        let dateRange = [];
+        while (start.isSameOrBefore(end)) {
+            dateRange.push({
+                date: start.format('DD'),
+                dayOfWeek: start.format('ddd')
+            });
+            start = start.add(1, 'day');
+        }
+        this.setState({
+            last7days: dateRange,
+            selectedDate: dateRange.length - 1
+        });
+    }
+
+    onChangeDate = (i) => {
+        this.setState({
+            selectedDate: i
+        });
+    }
     render() {
+        const { last7days, selectedDate } = this.state;
         return (
             <View style={[
                 Helpers.fill,
                 Metrics.verticalPadding,
                 Metrics.horizontalPadding,
                 styles.container]}>
-                <View>
-                    <Text>This is history screen</Text>
+                <View style={[styles.dateRangeContainer]}>
+                    {last7days.map((v, i) => (
+                        <TouchableWithoutFeedback onPress={() => this.onChangeDate(i)}>
+                            <View style={[
+                                styles.dateContainer(selectedDate === i),
+                            ]}>
+                                <Text
+                                    key={i}
+                                    style={[styles.dateText(selectedDate === i)]}
+                                >{v.date}</Text>
+                                <Text
+                                    key={i}
+                                    style={[styles.dateText(selectedDate === i)]}
+                                >{v.dayOfWeek}</Text>
+                                {last7days.length - 1 === i &&
+                                    <View style={[styles.circle]}></View>}
+                            </View>
+                        </TouchableWithoutFeedback>
+                    ))}
                 </View>
             </View>
         )
--- App/Containers/History/indexStyle.js
+++ App/Containers/History/indexStyle.js
@@ -1,8 +1,32 @@
 import { StyleSheet } from 'react-native'
-import { Colors } from 'App/Theme'
+import { Colors, Helpers, Metrics, Fonts } from 'App/Theme'
 
 export default StyleSheet.create({
     container: {
         backgroundColor: Colors.primary,
+    },
+    dateRangeContainer: {
+        ...Helpers.rowCross,
+        ...Helpers.mainSpaceBetween,
+    },
+    dateContainer: selectedDate => ({
+        ...Helpers.center,
+        ...Metrics.tinyVerticalPadding,
+        ...Metrics.tinyHorizontalPadding,
+        ...Metrics.tinyBorderRadius,
+        backgroundColor: selectedDate ? Colors.activeTint : Colors.transparent,
+    }),
+    dateText: selectedDate => ({
+        ...Fonts.normal,
+        color: selectedDate ? Colors.white : Colors.activeTint,
+    }),
+    circle: {
+        position: 'absolute',
+        right: -2,
+        top: -2,
+        width: 10,
+        height: 10,
+        backgroundColor: Colors.red,
+        borderRadius: 5
     },
 })
